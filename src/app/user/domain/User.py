82cac58baa744c66c9d8a5351ef8244d054e9f5f
Carlos Uscamayta <=>
2023-03-17 15:04:42 -0400

    updated user domain and getUser adapted

--- src/app/user/domain/User.py
+++ src/app/user/domain/User.py
@@ -1,5 +1,8 @@
+from bson import ObjectId
+
 class User:
     def __init__(self, payload):
+        self._id = str(payload["_id"]) if "_id" in payload else None
         self.name = payload["name"]
         self.phone = payload["phone"]
         self.credential = payload["credential"]
@@ -14,4 +17,18 @@ class User:
             "role": self.role,
             "username": self.username,
         }
+        if self._id != None:
+            response["_id"] = self._id
+        return response
+    
+    def toBson(self):
+        response = {
+            "name": self.name,
+            "phone": self.phone,
+            "credential": self.credential,
+            "role": self.role,
+            "username": self.username,
+        }
+        if self._id != None:
+            response["_id"] = ObjectId(self._id)
         return response
